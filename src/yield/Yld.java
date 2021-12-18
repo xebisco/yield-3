0ae725f795df4f75c63215f7bd3db2fb46a87f77
vto Vítor <87500917+vtogames@users.noreply.github.com>
2021-12-17 23:51:29 -0300

    update Yld.java

--- src/yield/Yld.java
+++ src/yield/Yld.java
@@ -4,7 +4,7 @@ import yield.display.YldFrame;
 
 public final class Yld {
 
-    public static final String YIELD_VERSION = "3.8 Public Beta 7";
+    public static final String YIELD_VERSION = "3.8 Public Beta 8";
 
     private static YldGame mainGame;
 
